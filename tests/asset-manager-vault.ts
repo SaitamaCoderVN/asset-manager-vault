1ff26120c28ae9a9fde65123bbf2189eb408608f
SaitamaCoder <112191530+SaitamaCoderVN@users.noreply.github.com>
2024-09-24 00:09:59 +0700

    Update asset-manager-vault.ts

--- tests/asset-manager-vault.ts
+++ tests/asset-manager-vault.ts
@@ -1,159 +1,460 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { AssetManagerVault } from "../target/types/asset_manager_vault";
-import { PublicKey, Keypair } from "@solana/web3.js";
-import bs58 from 'bs58';
 import {
-  Account,
+  workspace,
+  setProvider,
+  AnchorProvider,
+  Program,
+  getProvider,
+  Provider,
+} from "@coral-xyz/anchor";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  RpcResponseAndContext,
+  SignatureResult,
+} from "@solana/web3.js";
+import { before } from "mocha";
+import { S3AssetManagerVault } from "../target/types/s_3_asset_manager_vault";
+import {
+  createAssociatedTokenAccount,
   createMint,
   getAccount,
-  getOrCreateAssociatedTokenAccount,
   mintTo,
-} from '@solana/spl-token';
+} from "@solana/spl-token";
+import { BN } from "bn.js";
+import { expect } from "chai";
 
-// private key of alice, that is customer
-const PRIVATE_KEY_ALICE = "<YOUR_PRIVATE_KEY>";
+setProvider(AnchorProvider.env());
 
-// confirm transaction
-async function confirmTransaction(tx: string) {
-  const latestBlockHash = await anchor.getProvider().connection.getLatestBlockhash();
-  await anchor.getProvider().connection.confirmTransaction({
-    blockhash: latestBlockHash.blockhash,
-    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-    signature: tx,
-  });
-}
+const program = workspace.S3AssetManagerVault as Program<S3AssetManagerVault>;
+const provider = getProvider();
+const PDA_VAULT_SEED = "vault";
+const PDA_CUSTOMER_VAULT_ACCOUNT_SEED = "customer";
+const TOKEN_DECIMALS = 3;
 
-// import wallet from private key
-async function importWalletFromPrivateKey(privateKeyString: string): Promise<Keypair> {
-  const privateKey = bs58.decode(privateKeyString);
-  const wallet = Keypair.fromSecretKey(privateKey);
-  console.log("    Imported wallet:", wallet.publicKey.toString());
-  return wallet;
-}
+const manager = Keypair.generate();
+const mintKeypair = Keypair.generate();
+const mint2Keypair = Keypair.generate();
 
-// get pda
-async function getVaultPda(
-  program: anchor.Program<AssetManagerVault>,
-  tag: string,
-  mintAccount: { toBuffer: () => Uint8Array | Buffer; }
-): Promise<{ pubkey: PublicKey; bump: number }> {
-  const [pubKey, bump] = await PublicKey.findProgramAddressSync(
-    [Buffer.from(tag), mintAccount.toBuffer()],
+let mint: PublicKey;
+let mint2: PublicKey;
+
+let customer: Keypair;
+let customerATA: PublicKey;
+let customerATA2: PublicKey;
+
+const confirmTransaction = async (
+  provider: Provider,
+  tx: string
+): Promise<RpcResponseAndContext<SignatureResult>> => {
+  const bh = await provider.connection.getLatestBlockhash();
+
+  return await provider.connection.confirmTransaction(
+    {
+      signature: tx,
+      blockhash: bh.blockhash,
+      lastValidBlockHeight: bh.lastValidBlockHeight,
+    },
+    "confirmed"
+  );
+};
+
+const getVaultPda = (): PublicKey => {
+  const [vaultPda, vaultPdaBumpState] = PublicKey.findProgramAddressSync(
+    [Buffer.from(PDA_VAULT_SEED)],
     program.programId
   );
-  return { pubkey: pubKey, bump };
-}
 
-// log transaction
-async function logTransaction(connection: anchor.web3.Connection, txHash: string) {
-  const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
-  await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature: txHash });
-  console.log(`    https://solscan.io/tx/${txHash}?cluster=devnet`);
-}
+  return vaultPda;
+};
 
-// Test suite
-describe("asset manager vault", () => {
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
-
-  const connection = provider.connection;
-  const program = anchor.workspace.AssetManagerVault as Program<AssetManagerVault>;
-
-  let walletAlice: Keypair;
-  let mintAlice: PublicKey;
-  let ataAlice: Account;
-
-  const TOKEN_DECIMALS = 6;
-  const INITIAL_MINT_AMOUNT = 1000 * 10 ** TOKEN_DECIMALS;
-
+describe("s3-asset-manager-vault", () => {
   before(async () => {
-    // import wallet from private key
-    walletAlice = await importWalletFromPrivateKey(PRIVATE_KEY_ALICE);
-
-    // create mint account
-    mintAlice = await createMint(
-      connection,
-      walletAlice,
-      walletAlice.publicKey,
-      null,
-      TOKEN_DECIMALS
+    const aridropTx = await program.provider.connection.requestAirdrop(
+      manager.publicKey,
+      5 * LAMPORTS_PER_SOL
     );
-    console.log("    mint Alice", mintAlice.toBase58());
 
-    // create ata
-    ataAlice = await getOrCreateAssociatedTokenAccount(
-      connection,
-      walletAlice,
-      mintAlice,
-      walletAlice.publicKey
-    );
-    console.log("    ATA Alice", ataAlice.address.toBase58());
+    const confirmation = await confirmTransaction(provider, aridropTx);
 
-    // mint tokens
-    const mintTx = await mintTo(
-      connection,
-      walletAlice,
-      mintAlice,
-      ataAlice.address,
-      walletAlice,
-      INITIAL_MINT_AMOUNT
-    );
-    await confirmTransaction(mintTx);
-
-    // check balance
-    const tokenAccountInfo = await getAccount(connection, ataAlice.address);
-    const balanceToken = Number(tokenAccountInfo.amount) / 10 ** TOKEN_DECIMALS;
-    console.log("    balance token of Alice:", balanceToken);
-  });
-
-  it("Is initialized!", async () => {
-    const [tokenAccountOwnerPda] = PublicKey.findProgramAddressSync(
-      [Buffer.from("SPL_ACCOUNT_VAULT")],
-      program.programId
-    );
-    console.log("    TokenAccountOwnerPda:", tokenAccountOwnerPda.toString());
+    if (confirmation.value.err) {
+      throw confirmation.value.err;
+    }
 
     try {
-      // initialize vault by dev wallet on your devnet
-      const initVaultTx = await program.methods
-        .initialize()
-        .accounts({
-          signer: program.provider.publicKey,
-        })
-        .rpc({ skipPreflight: true });
+      mint = await createMint(
+        program.provider.connection,
+        manager,
+        manager.publicKey,
+        null,
+        TOKEN_DECIMALS,
+        mintKeypair
+      );
+    } catch (error) {
+      console.log("create mint failed");
+    }
 
-      await logTransaction(connection, initVaultTx);
-    } catch (err) {
-      console.error("Error when initializing:", err);
+    try {
+      mint2 = await createMint(
+        program.provider.connection,
+        manager,
+        manager.publicKey,
+        null,
+        TOKEN_DECIMALS,
+        mint2Keypair
+      );
+    } catch (error) {
+      console.log("create mint2 failed");
     }
   });
 
-  it("deposit(): multiple deposit on one unique vault", async () => {
-    // get vault pda
-    const pda = await getVaultPda(program, "SPL_PDA_VAULT", mintAlice);
-    const [tokenAccountOwnerPda] = PublicKey.findProgramAddressSync(
-      [Buffer.from("SPL_ACCOUNT_VAULT")],
-      program.programId
-    );
+  it("should initialize vault", async () => {
+    const tx = await program.methods
+      .initializeVault()
+      .accounts({
+        manager: manager.publicKey,
+      })
+      .signers([manager])
+      .rpc();
 
-    const depositAmount = (amount: number) => new anchor.BN(amount * 10 ** TOKEN_DECIMALS);
+    const confirmation = await confirmTransaction(provider, tx);
 
-    const depositAccounts = {
-      tokenAccountOwnerPda,
-      vault: pda.pubkey,
-      signer: walletAlice.publicKey,
-      mintAccount: mintAlice,
-      senderTokenAccount: ataAlice.address,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    };
-
-    for (const amount of [100, 200]) {
-      const tx = await program.methods.deposit(depositAmount(amount))
-        .accounts(depositAccounts)
-        .signers([walletAlice])
-        .rpc();
-      console.log(`    (deposit +${amount}) https://solscan.io/tx/${tx}?cluster=devnet\n`);
+    if (confirmation.value.err) {
+      throw confirmation.value.err;
     }
+  });
+
+  describe("customer tests", () => {
+    before(async () => {
+      customer = Keypair.generate();
+
+      const aridropTx = await program.provider.connection.requestAirdrop(
+        customer.publicKey,
+        5 * LAMPORTS_PER_SOL
+      );
+
+      const confirmation = await confirmTransaction(provider, aridropTx);
+
+      if (confirmation.value.err) {
+        throw confirmation.value.err;
+      }
+
+      try {
+        customerATA = await createAssociatedTokenAccount(
+          program.provider.connection,
+          customer,
+          mint,
+          customer.publicKey
+        );
+      } catch (error) {
+        console.log("create customerATA failed");
+      }
+
+      const mintToTx = await mintTo(
+        provider.connection,
+        customer,
+        mint,
+        customerATA,
+        manager,
+        BigInt(100 * Math.pow(10, TOKEN_DECIMALS))
+      );
+
+      const mintToConfirmation = await confirmTransaction(provider, mintToTx);
+
+      if (mintToConfirmation.value.err) {
+        throw mintToConfirmation.value.err;
+      }
+
+      try {
+        customerATA2 = await createAssociatedTokenAccount(
+          program.provider.connection,
+          customer,
+          mint2,
+          customer.publicKey
+        );
+      } catch (error) {
+        console.log("create customerATA failed");
+      }
+
+      const mintToTx2 = await mintTo(
+        provider.connection,
+        customer,
+        mint2,
+        customerATA2,
+        manager,
+        BigInt(100 * Math.pow(10, TOKEN_DECIMALS))
+      );
+
+      const mintToConfirmation2 = await confirmTransaction(provider, mintToTx2);
+
+      if (mintToConfirmation2.value.err) {
+        throw mintToConfirmation2.value.err;
+      }
+    });
+
+    afterEach(async () => {
+      const vaultPda = getVaultPda();
+
+      const [customerPDA, customerPdaBumpState] =
+        PublicKey.findProgramAddressSync(
+          [
+            vaultPda.toBytes(),
+            mint.toBytes(),
+            customer.publicKey.toBytes(),
+            Buffer.from(PDA_CUSTOMER_VAULT_ACCOUNT_SEED),
+          ],
+          program.programId
+        );
+
+      const customerAccount = await program.account.customerVaultAccount.fetch(
+        customerPDA
+      );
+
+      const ata = await getAccount(provider.connection, customerATA);
+
+      console.log("customer ata: ", ata.address.toBase58());
+      console.log("customer ata balance: ", ata.amount);
+
+      console.log(
+        "vault token account: ",
+        customerAccount.vaultTokenAccount.toBase58()
+      );
+      console.log(
+        "vault token acount balance: ",
+        customerAccount.balance.toNumber()
+      );
+
+      const [customerPDA2, customerPdaBumpState2] =
+        PublicKey.findProgramAddressSync(
+          [
+            vaultPda.toBytes(),
+            mint2.toBytes(),
+            customer.publicKey.toBytes(),
+            Buffer.from(PDA_CUSTOMER_VAULT_ACCOUNT_SEED),
+          ],
+          program.programId
+        );
+
+      try {
+        const customerAccount2 =
+          await program.account.customerVaultAccount.fetch(customerPDA2);
+
+        const ata2 = await getAccount(provider.connection, customerATA2);
+
+        console.log("customer ata2: ", ata2?.address.toBase58());
+        console.log("customer ata2 balance: ", ata2?.amount);
+
+        console.log(
+          "vault token account2: ",
+          customerAccount2?.vaultTokenAccount?.toBase58()
+        );
+        console.log(
+          "vault token acount balance2: ",
+          customerAccount2?.balance?.toNumber()
+        );
+      } catch (error) {
+        console.log("customerAccount2 doesn't exist");
+      }
+    });
+
+    it("should deposit tokens", async () => {
+      const vaultPda = getVaultPda();
+
+      const depositTx = await program.methods
+        .deposit(new BN(3.123 * Math.pow(10, TOKEN_DECIMALS)))
+        .accounts({
+          customer: customer.publicKey,
+          customerTokenAccount: customerATA,
+          mint: mint,
+          vault: vaultPda,
+        })
+        .signers([customer])
+        .rpc();
+
+      const depositTxConfirmation = await confirmTransaction(
+        provider,
+        depositTx
+      );
+
+      if (depositTxConfirmation.value.err) {
+        throw depositTxConfirmation.value.err;
+      }
+    });
+
+    it("should deposit tokens into same account", async () => {
+      const vaultPda = getVaultPda();
+
+      const depositTx = await program.methods
+        .deposit(new BN(3.123 * Math.pow(10, TOKEN_DECIMALS)))
+        .accounts({
+          customer: customer.publicKey,
+          customerTokenAccount: customerATA,
+          mint: mint,
+          vault: vaultPda,
+        })
+        .signers([customer])
+        .rpc();
+
+      const depositTxConfirmation = await confirmTransaction(
+        provider,
+        depositTx
+      );
+
+      if (depositTxConfirmation.value.err) {
+        throw depositTxConfirmation.value.err;
+      }
+    });
+
+    it("should withdraw tokens", async () => {
+      const vaultPda = getVaultPda();
+
+      const withdrawTx = await program.methods
+        .withdraw(new BN(2.112 * Math.pow(10, TOKEN_DECIMALS)))
+        .accounts({
+          customer: customer.publicKey,
+          customerTokenAccount: customerATA,
+          mint: mint,
+          vault: vaultPda,
+        })
+        .signers([customer])
+        .rpc();
+
+      const depositTxConfirmation = await confirmTransaction(
+        provider,
+        withdrawTx
+      );
+
+      if (depositTxConfirmation.value.err) {
+        throw depositTxConfirmation.value.err;
+      }
+    });
+
+    it("should fail due to insuficient funds", async () => {
+      const vaultPda = getVaultPda();
+
+      try {
+        const withdrawTx = await program.methods
+          .withdraw(new BN(12.232 * Math.pow(10, TOKEN_DECIMALS)))
+          .accounts({
+            customer: customer.publicKey,
+            customerTokenAccount: customerATA,
+            mint: mint,
+            vault: vaultPda,
+          })
+          .signers([customer])
+          .rpc();
+
+        const depositTxConfirmation = await confirmTransaction(
+          provider,
+          withdrawTx
+        );
+
+        if (depositTxConfirmation.value.err) {
+          throw depositTxConfirmation.value.err;
+        }
+      } catch (error) {
+        expect(error.error.errorCode.number as number).to.equal(6003);
+      }
+    });
+
+    it("should deposit tokens", async () => {
+      const vaultPda = getVaultPda();
+
+      const depositTx = await program.methods
+        .deposit(new BN(3.123 * Math.pow(10, TOKEN_DECIMALS)))
+        .accounts({
+          customer: customer.publicKey,
+          customerTokenAccount: customerATA2,
+          mint: mint2,
+          vault: vaultPda,
+        })
+        .signers([customer])
+        .rpc();
+
+      const depositTxConfirmation = await confirmTransaction(
+        provider,
+        depositTx
+      );
+
+      if (depositTxConfirmation.value.err) {
+        throw depositTxConfirmation.value.err;
+      }
+    });
+
+    it("should deposit tokens into same account", async () => {
+      const vaultPda = getVaultPda();
+
+      const depositTx = await program.methods
+        .deposit(new BN(3.123 * Math.pow(10, TOKEN_DECIMALS)))
+        .accounts({
+          customer: customer.publicKey,
+          customerTokenAccount: customerATA2,
+          mint: mint2,
+          vault: vaultPda,
+        })
+        .signers([customer])
+        .rpc();
+
+      const depositTxConfirmation = await confirmTransaction(
+        provider,
+        depositTx
+      );
+
+      if (depositTxConfirmation.value.err) {
+        throw depositTxConfirmation.value.err;
+      }
+    });
+
+    it("should withdraw tokens", async () => {
+      const vaultPda = getVaultPda();
+
+      const withdrawTx = await program.methods
+        .withdraw(new BN(2.112 * Math.pow(10, TOKEN_DECIMALS)))
+        .accounts({
+          customer: customer.publicKey,
+          customerTokenAccount: customerATA2,
+          mint: mint2,
+          vault: vaultPda,
+        })
+        .signers([customer])
+        .rpc();
+
+      const depositTxConfirmation = await confirmTransaction(
+        provider,
+        withdrawTx
+      );
+
+      if (depositTxConfirmation.value.err) {
+        throw depositTxConfirmation.value.err;
+      }
+    });
+
+    it("should fail due to insuficient funds", async () => {
+      const vaultPda = getVaultPda();
+
+      try {
+        const withdrawTx = await program.methods
+          .withdraw(new BN(12.232 * Math.pow(10, TOKEN_DECIMALS)))
+          .accounts({
+            customer: customer.publicKey,
+            customerTokenAccount: customerATA2,
+            mint: mint2,
+            vault: vaultPda,
+          })
+          .signers([customer])
+          .rpc();
+
+        const depositTxConfirmation = await confirmTransaction(
+          provider,
+          withdrawTx
+        );
+
+        if (depositTxConfirmation.value.err) {
+          throw depositTxConfirmation.value.err;
+        }
+      } catch (error) {
+        expect(error.error.errorCode.number as number).to.equal(6003);
+      }
+    });
   });
 });
